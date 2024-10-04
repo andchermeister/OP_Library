dfa76722784f3a610a09f475de0d45c7e1645449
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2024-10-04 14:19:39 +0100

    Changed functions with classes instead

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-const myLibrary = [];
+class Book {
+    constructor(title, author, numOfPgs, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.numOfPgs = numOfPgs;
+        this.readStatus = readStatus;
+    };
 
-function Book(title, author, numOfPgs, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.numOfPgs = numOfPgs;
-    this.readStatus = readStatus;
-};
-
-Book.prototype.toggleRead = function() {
-    this.readStatus = !this.readStatus;
+    toggleRead() {
+        this.readStatus = !this.readStatus;
+    };
 }
 
-function toggleRead(index) {
-    myLibrary[index].toggleRead();
-    displayBooks();
-}
+class Library {
+    constructor() {
+        this.myLibrary = [];
+    };
 
-function addBookToLibrary(title, author, numOfPgs, readStatus) {
-    const book = new Book(title, author, numOfPgs, readStatus);
-    myLibrary.push(book);
-    displayBooks();
+    addBookToLibrary(title, author, numOfPgs, readStatus) {
+        const book = new Book(title, author, numOfPgs, readStatus);
+        this.myLibrary.push(book);
+        this.displayBooks();
+    };
+
+    displayBooks() {
+        const libraryList = document.getElementById('libraryList');
+        libraryList.innerHTML = '';
+    
+        this.myLibrary.forEach((book, index) => {
+            const bookItem = document.createElement('div');
+            bookItem.classList.add('book-item');
+            bookItem.innerHTML = `
+                <b>Title:</b> ${book.title} <br>
+                <b>Author:</b> ${book.author} <br>
+                <b>Number of pages:</b> ${book.numOfPgs} <br>
+                <b>Read status:</b> ${book.readStatus ? "Read" : "Not read"} <br>
+                <button onclick="library.removeBook(${index})">Remove</button>
+                <button onclick="library.toggleRead(${index})">Toggle Read</button>
+            `;
+            libraryList.appendChild(bookItem);
+        });
+    };
+
+    toggleRead(index) {
+        this.myLibrary[index].toggleRead();
+        this.displayBooks();
+    };
+
+    removeBook(index) {
+        this.myLibrary.splice(index, 1);
+        this.displayBooks();
+    };
+
+
 };
 
-function displayBooks() {
-    const libraryList = document.getElementById('libraryList');
-    libraryList.innerHTML = '';
-
-    myLibrary.forEach((book, index) => {
-        const bookItem = document.createElement('div');
-        bookItem.classList.add('book-item');
-        bookItem.innerHTML = `
-            <b>Title:</b> ${book.title} <br>
-            <b>Author:</b> ${book.author} <br>
-            <b>Number of pages:</b> ${book.numOfPgs} <br>
-            <b>Read status:</b> ${book.readStatus ? "Read" : "Not read"} <br>
-            <button onclick="removeBook(${index})">Remove</button>
-            <button onclick="toggleRead(${index})">Toggle Read</button>
-        `;
-        libraryList.appendChild(bookItem);
-    });
-};
-
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks();
-};
-
+const library = new Library();
 
 document.getElementById('bookForm').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -55,7 +64,7 @@ document.getElementById('bookForm').addEventListener('submit', function(event) {
     const numOfPgs = document.getElementById('pages').value;
     const read = document.getElementById('read').checked;
 
-    addBookToLibrary(title, author, numOfPgs, read);
+    library.addBookToLibrary(title, author, numOfPgs, read);
     this.reset();
 });
 
